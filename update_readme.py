9a8c2a98be05023d0fc29aaeb4841882be52981d
Isabela Siqueira <isabela7011@gmail.com>
2025-12-27 16:44:49 -0300

    add enconding

--- update_readme.py
+++ update_readme.py
@@ -5,10 +5,10 @@ from datetime import datetime
 def update_readme():
     days_left = (datetime(2026, 1, 1) - datetime.now()).days
 
-    with open("README.md", "r") as file:
+    with open("README.md", "r", encoding="utf-8") as file:
         content = file.read()
 
     updated_content = re.sub(r"(There are )\d+( days remaining in 2025)", rf"\g<1>{days_left}\g<2>", content, count=1)
 
-    with open("README.md", "w") as file:
+    with open("README.md", "w", encoding="utf-8") as file:
         file.write(updated_content)
